580fe9fb1ea5c70aa1dc8596326613d87591ee14
Eldar Aliyev <eldar.aliyev8@gmail.com>
2013-09-30 20:12:50 +0500

    merged

--- src/lib/Tests/DovecotHmacmd5Test.php
+++ src/lib/Tests/DovecotHmacmd5Test.php
@@ -8,17 +8,11 @@
  */
 
 class DovecotHmacmd5 extends PHPUnit_Framework_TestCase {
-<<<<<<< HEAD
 
-=======
->>>>>>> 9f40a6e9d48399c2a42a1a8afc1c4be633870b81
     public function testDovecotHmacmd5(){
         $password = 'ifgrfc';
         $hashedPassword = '{HMAC-MD5}52903d01cd334d86e33eeef850de9f2096ebb6cbd9597b2c0adbc1535698030c';
         $this->assertEquals($hashedPassword, dovecot_hmacmd5($password));
     }
-<<<<<<< HEAD
 
-=======
->>>>>>> 9f40a6e9d48399c2a42a1a8afc1c4be633870b81
 }
